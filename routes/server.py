1de5717a1613878a705528560fb529496ed386b1
BenoitHage <hage.benoit@gmail.com>
2022-06-06 00:41:54 +0200

    first commit

--- routes/server.py
+++ routes/server.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, render_template, redirect, session, request, flash, get_flashed_messages
 from application import getUserName, getUserPicture, login_required, confirmed_required, getUserRole, getUserIp, getUserPort
-from pythonping import ping
 
 
 # Set Blueprints
@@ -15,12 +14,6 @@ def serverFunction():
 
     # Force flash() to get the messages on the same page as the redirect.
     get_flashed_messages()
-
-
-    def test():
-        return ping(getUserIp(), verbose=True)
-
-    test()
 
 
     if request.method == "POST":
