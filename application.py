cde15d3a38dab6dfc3e833bcd75dec9e37dc7173
BenoitHage <hage.benoit@gmail.com>
2022-08-04 13:17:06 +0200

    first commit

--- application.py
+++ application.py
@@ -455,11 +455,10 @@ def handle_time(data):
 # SocketIO server side event handler for /chat
 @socketio.on("chatMessage")
 def handle_message(data):
-    temp = type(data)
-    print(temp)
-    ## extension = [getUserName, getUserPicture]
-    ## data.extend(extension)
-    ## print(data)
+    extension = [getUserName, getUserPicture]
+    print(extension)
+    data.extend(extension)
+    print(data)
     emit("chatResponse", data, broadcast=True)
 
 
