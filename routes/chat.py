1a086440be15a30a87ab3517a9cb8a75084112c2
Benoit Hage <benoit@Benoits-MacBook-Air.local>
2022-09-26 18:01:50 +0200

    first commit

--- routes/chat.py
+++ routes/chat.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, redirect, session, request, flash, get_flashed_messages
-from application import getUserName, getUserPicture, getUserRole, getUserRoom, login_required, confirmed_required, db, Users
+from application import getUserName, getUserPicture, getUserRole, getUserRoom, getUserRooms, login_required, confirmed_required, db, Users
 
 
 # Set Blueprints
@@ -22,4 +22,7 @@ def chatFunction():
 
     else:
 
-        return render_template("chat.html", name=getUserName(), picture=getUserPicture(), role=getUserRole()) 
+        myrooms = getUserRoom
+        rooms= getUserRooms
+
+        return render_template("chat.html", name=getUserName(), picture=getUserPicture(), role=getUserRole(), myrooms=myrooms, rooms=rooms) 
