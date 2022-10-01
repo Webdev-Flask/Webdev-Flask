68384ccbae11287899f0fbcb9622935fde3a6c58
Benoit Hage <benoit@Benoits-MacBook-Air.local>
2022-10-01 22:00:39 +0200

    first commit

--- application.py
+++ application.py
@@ -354,7 +354,7 @@ def getUserRooms():
     loggedId = session["user_id"]
         
     # Query database for all room lists
-    query = Users.query.filter(Users.id != id).all()
+    query = Users.query.all()
 
     # Set variables
     temporary = []
